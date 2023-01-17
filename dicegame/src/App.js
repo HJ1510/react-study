d1606996ba90d7dcdf22c201c69a1baf66e584f5
HJ1510 <yhj221004@gmail.com>
2023-01-17 17:55:25 +0900

    react Day1

---
+++ dicegame/src/App.js
@@ -0,0 +1,13 @@
+import Dice from "./Dice";
+import HandIcon from "./HandIcon";
+
+function App() {
+  return (
+    <div>
+      <HandIcon />
+      <Dice />      
+    </div>
+  );
+}
+
+export default App;
