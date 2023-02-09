a15851cfa0176389a45e360b02a5db972d6246e5
HJ1510 <yhj221004@gmail.com>
2023-02-09 15:44:56 +0900

    state 한번에 관리(input name)

--- moviepedia/src/component/ReviewForm.js
+++ moviepedia/src/component/ReviewForm.js
@@ -2,45 +2,42 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (input) => {
-    setTitle(input.target.value);
-  };
-
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault(); // preventDefault(): 기본 동작 막는 함수
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(values);
   };
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input value={title} placeholder="title" onChange={handleTitleChange} />
       <input
+        name="title"
+        value={values.title}
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <input
+        name="rating"
         type="number"
-        value={rating}
+        value={values.rating}
         placeholder="rating"
-        onChange={handleRatingChange}
+        onChange={handleChange}
       />
       <textarea
-        value={content}
+        name="content"
+        value={values.content}
         placeholder="content"
-        onChange={handleContentChange}
+        onChange={handleChange}
       />
       <button type="sumbit">확인</button>{" "}
       {/** type="sumbit" -> onSubmit 이벤트 발생 */}
