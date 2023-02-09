04b0887d4eb63ef64e46285420582a0ad57213df
HJ1510 <yhj221004@gmail.com>
2023-02-09 15:34:34 +0900

    onSubmit,preventDefault

--- moviepedia/src/component/ReviewForm.js
+++ moviepedia/src/component/ReviewForm.js
@@ -19,8 +19,17 @@ function ReviewForm() {
     setContent(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // preventDefault(): 기본 동작 막는 함수
+    console.log({
+      title,
+      rating,
+      content,
+    });
+  };
+
   return (
-    <form className="ReviewForm">
+    <form className="ReviewForm" onSubmit={handleSubmit}>
       <input value={title} placeholder="title" onChange={handleTitleChange} />
       <input
         type="number"
@@ -33,6 +42,8 @@ function ReviewForm() {
         placeholder="content"
         onChange={handleContentChange}
       />
+      <button type="sumbit">확인</button>{" "}
+      {/** type="sumbit" -> onSubmit 이벤트 발생 */}
     </form>
   );
 }
