71afcf55724d77a7d80e715a10cc9d4f00ca02af
HJ1510 <yhj221004@gmail.com>
2023-02-09 17:44:09 +0900

    ref

--- moviepedia/src/component/FileInput.js
+++ moviepedia/src/component/FileInput.js
@@ -1,9 +1,18 @@
+import { useEffect, useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
   const handleInputChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
-  return <input type="file" onChange={handleInputChange} />;
+
+  useEffect(() => {
+    if (inputRef.current) {
+      console.log(inputRef.current);
+    }
+  }, []);
+  return <input type="file" onChange={handleInputChange} ref={inputRef} />;
 }
 
 export default FileInput;
